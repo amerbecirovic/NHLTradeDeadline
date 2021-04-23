c3048c2de9bbcf5420e197f3b40c54f376cee7f0
Amer Becirovic <amer@MacBook-Air.local>
2021-04-23 11:25:30 -0600

    first commit

---
+++ NHLTradeDeadline.py
@@ -0,0 +1,4 @@
+from Team import Team
+
+bruins = Team("Boston Bruins")
+print(bruins.get_team_name())
---
+++ Team.py
@@ -0,0 +1,7 @@
+class Team:
+    def __init__(self, team_name):
+        self.team_name = team_name
+
+    def get_team_name(self):
+        return self.team_name
+
